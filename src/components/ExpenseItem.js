8a07a4dd566c369e696419f40734e979d6cb0e93
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-01-14 23:03:09 +0530

    adding dynamic data to components using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>January 5th 2023</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
+      <div>
+        <div>{props.date.toLocaleString("en-us", { month: "long" })}</div>
+        <div>{props.date.toLocaleString("en-us", { day: "2-digit" })}</div>
+        <div>{props.date.getFullYear()}</div>
       </div>
-      <div className="expense-item__price">$294.67</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   );
 }
