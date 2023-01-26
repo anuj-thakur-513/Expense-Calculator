d6128b8308ab7778ca27d87c7374ce8a98371089
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-01-26 21:46:22 +0530

    Changing the functionality of add feature

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from "react";
+import { useState } from "react";
 
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   // function to handle the input entered in the child form ExpenseForm
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
@@ -12,11 +14,28 @@ const NewExpense = (props) => {
     };
     // calling the function from parent component app
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
